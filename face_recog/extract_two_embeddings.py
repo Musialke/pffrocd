0827a51d270bc6407cab922b7e65e9b154a69e65
Musiał, K <kmusial98@gmail.com>
2023-07-06 13:59:53 +0200

    add jupyter notebooks

--- face_recog/extract_two_embeddings.py
+++ face_recog/extract_two_embeddings.py
@@ -6,16 +6,20 @@ import argparse
 
 parser = argparse.ArgumentParser(
                     prog='Extract Embedding',
-                    description='Extracts the face features vectors from an image and prints the output, or saves it to a file')
+                    description='Extracts the face features vectors from two images to be compared')
 
 parser.add_argument('imagepath1')
+parser.add_argument('imagepath2')
 parser.add_argument('--output', help="output file")
 args = parser.parse_args()
 embedding1 = DeepFace.represent(img_path = args.imagepath1, model_name="SFace", enforce_detection=True)[0]["embedding"]
+embedding2 = DeepFace.represent(img_path = args.imagepath2, model_name="SFace", enforce_detection=True)[0]["embedding"]
 assert isinstance(embedding1, list)
+assert isinstance(embedding2, list)
 if (args.output):
     with open(args.output, 'w') as f:
-        for x in embedding1:
-            f.write(f"{x}\n")
+        for x, y in zip(embedding1, embedding2):
+            f.write(f"{x} {y}\n")
 else: 
-    print(embedding1)
+    print(embedding1, embedding2)
+
