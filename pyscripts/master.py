58d9c3a3d1fa140b8fee50fc5f27ed3cfa3f2e45
Musiał, K <kmusial98@gmail.com>
2023-09-14 10:29:53 +0200

    fix sudo password asking

--- pyscripts/master.py
+++ pyscripts/master.py
@@ -180,7 +180,8 @@ def run_test():
             logger.debug(f"{parsed_sfe_output=}")
             logger.debug(f"{list_of_sfe_values=}")
             # if ram_usage asked for password for sudo delete that entry
-            if ram_usage[f'[sudo] password for {server_username}']:
+            logger.debug(f"Checking if the key <[sudo] password for {server_username}> exists")
+            if ram_usage[f'[sudo] password for {server_username}'] == '':
                 del ram_usage[f'[sudo] password for {server_username}']
             list_of_ram_values = list(ram_usage.values()) # [1:] # remove the first element, asking for sudo
             logger.debug(f"{ram_usage=}")
